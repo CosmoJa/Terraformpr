c8f9c5d09ab5d65604fa36cf2e87fd27fa70763e
“CosmoJa” <zhyldyz.inquiries@gmail.com>
2024-03-18 18:54:57 -0400

    added

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket                  = "random-bucket-cosmo"
+    key                     = "my-terraform-project"
+    region                  = "us-east-2"
+    # shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
+provider "aws"{
+  region = "us-east-2"
+}
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "test"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
